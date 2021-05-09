dc9c5c0b05361b327774b5a9b320b5172eb7240f
PercivallAkihiko <37913210+PercivallAkihiko@users.noreply.github.com>
2021-05-09 09:40:26 +0200

    Pulito fine_partita.php

--- php/fine_partita.php
+++ php/fine_partita.php
@@ -2,7 +2,7 @@
      $dbconn = pg_connect("host=localhost port=5432 dbname=progetto user=postgres password=ciaomondo") or die("Could not connect: " . pg_last_error());
      $streak = $_POST["streak1"];
      $username= $_POST['username'];
-     $query="select total_matches from users where username= $1";
+     $query="select * from users where username= $1";
      $result=pg_query_params($dbconn, $query, array($username));
 
 
@@ -11,35 +11,19 @@
      }
      else 
      {
-          foreach ($line as $col_value)
-          {
-               $partite_giocate_nuovo = $col_value+1;
-          }
+          $highscore = $line['highscore'];
+          $partite_giocate = $line['total_matches'];
      }
-     $query="UPDATE users SET total_matches='$partite_giocate_nuovo' WHERE username='$username'";
+
+     if($streak > $highscore) $highscore = $streak;
+     $partite_giocate += 1;
+
+     $query="UPDATE users SET total_matches='$partite_giocate', highscore='$highscore' WHERE username='$username'";
      $result=pg_query($dbconn,$query);
 
-     $query="select highscore from users where username= $1";
-     $result=pg_query_params($dbconn, $query, array($username));
-
-     if (!($line=pg_fetch_array($result, null, PGSQL_ASSOC))){
-          echo "errore!";                
-     }
-     else 
-     {
-          foreach ($line as $col_value)
-          {
-               $higscore_vecchio =  $col_value;
-          }
-     }
-
-     if ($streak > $higscore_vecchio)
-     {
-          $query="UPDATE users SET highscore='$streak' WHERE username='$username'";
-          $result=pg_query($dbconn,$query);
-     }
      pg_free_result($result);
      pg_close($dbconn);
-     header("Location: ../menu.php");
 
+     $link = $_POST['link'];
+     header("location: $link");
 ?>
