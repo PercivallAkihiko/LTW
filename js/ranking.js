4fb6bb45329bf4ad7c9088f37367ec2ed605aa47
PercivallAkihiko <37913210+PercivallAkihiko@users.noreply.github.com>
2021-05-10 13:01:20 +0200

    -

--- js/ranking.js
+++ js/ranking.js
@@ -12,6 +12,10 @@ window.onload = function () {
                 method: 'POST',
                 success: function (data) {
                     self.users = JSON.parse(data);
+
+                    self.users[0]["medal"] = "assets/medal/gold.png";
+                    self.users[1]["medal"] = "assets/medal/silver.png";
+                    self.users[2]["medal"] = "assets/medal/bronze.png";
                 },
                 error: function (error) {
                     console.log(error);
