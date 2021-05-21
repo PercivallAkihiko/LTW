3feeffa54ca7f017572a6f7e4b53aba1bf663e1b
Quelehir <mkryston2@gmail.com>
2021-05-21 20:38:59 +0200

    correzione tutorial!

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,9 +1,8 @@
-$(document).ready(function () {
-
+$(document).ready(function()
+{
     var username = localStorage.getItem("username");
-    var nationality = localStorage.getItem("nationality")
-
-    if (username !== null){
+    if (username != null)
+    {
         document.getElementById("button").innerHTML = "MENU";
     }
  });
